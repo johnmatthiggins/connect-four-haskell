b156305afcfce9b6c582406e10401886bb5da51a
John Higgins <johnmatthiggins@gmail.com>
2022-11-01 12:54:26 -0700

    Trying to create array argoesuntahosnethu

--- main.hs
+++ main.hs
@@ -1,2 +1,19 @@
+data Color = Red | Blue
+
 main :: IO ()
-main = putStrLn "Hello World"
+main = do
+    let board = newBoard
+    print (board :: [[Maybe Color c]])
+    return ()
+
+newBoard = [
+    [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
+    [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
+    [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
+    [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
+    [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
+    [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]]
+
+-- find valid spaces
+-- valid space
+-- board
